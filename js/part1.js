814a9d40fe99970b1a5f3067dd3adf0421b9e23f
jackhoppus <55254910+jackhoppus@users.noreply.github.com>
2020-01-19 19:42:31 -0800

    Update part1.js
    worked on base 10 into base N.
    
    returning 0, followed by all the desired answer digits except for the last one, in reverse order.

--- js/part1.js
+++ js/part1.js
@@ -1,21 +1,61 @@
 function ConversionPart1() {
 
-    var UnsignedInt = parseInt(document.getElementById("1_UnsignedInt").value);
+    var UnsignedInt = document.getElementById("1_UnsignedInt").value;
     var UnsignedIntBaseFrom = parseInt(document.getElementById("1_UnsignedIntBaseToConvertFrom").value);
     var UnsignedIntBaseTo = parseInt(document.getElementById("1_UnsignedIntBaseToConvertTo").value);
 
-
-    if (UnsignedIntBaseFrom != 10){
+      var outputValue = UnsignedInt;
       //convert to 10
       var value = 0;
       var workingInt = UnsignedInt;
+      var counter = 0;
+      while (counter <= UnsignedInt.length - 1){
+          var digit = UnsignedInt.substring(counter, counter + 1);
+          value = (value * UnsignedIntBaseFrom) + Number(digit);
+          counter++;
+      }
+      outputValue = value;
 
-    }
+      //converting from 10 into whatever desired base
+      //Value := value to be converted;
+      // REPEAT
+      //      Next digit of result := Value MOD N;
+      //      Value := Value DIV N;
+      // UNTIL (Value = 0);
+
+      var newFinalValue = 0;
+      var newWorkingValue = outputValue;
+      while (newWorkingValue != 0){
+        var nextDigit = newWorkingValue % UnsignedIntBaseTo;
+        newWorkingValue = newWorkingValue / UnsignedIntBaseTo;
+        newWorkingValue = newWorkingValue - nextDigit/UnsignedIntBaseTo;
+        if (newWorkingValue < 1){
+          newWorkingValue = 0;
+        }
+        if (nextDigit == 10){
+          nextDigit = 'A';
+        }
+        else if (nextDigit == 11){
+          nextDigit = 'B';
+        }
+        else if (nextDigit == 12){
+          nextDigit = 'C';
+        }
+        else if (nextDigit == 13){
+          nextDigit = 'D';
+        }
+        else if (nextDigit == 14){
+          nextDigit = 'E';
+        }
+        else if (nextDigit == 15){
+          nextDigit = 'F';
+        }
+        newFinalValue = newFinalValue + nextDigit;
+      }
 
 
-  var outputValue = "11111111";
 
   // Show the output on the screen
-  FormatAndShowOutput([UnsignedInt, UnsignedIntBaseFrom, UnsignedIntBaseTo, outputValue], 1);
+  FormatAndShowOutput([UnsignedInt, UnsignedIntBaseFrom, UnsignedIntBaseTo, newFinalValue], 1);
 
 }
